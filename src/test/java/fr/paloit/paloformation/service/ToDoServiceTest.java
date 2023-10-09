37f4607146a99180d77492fb808e02c3f7b4d9a1
Pandora Pierrot <pandora.pierrot@gmail.com>
2023-10-09 16:24:34 +0200

    Restructuration des tests

--- src/test/java/fr/paloit/paloformation/service/ToDoServiceTest.java
+++ src/test/java/fr/paloit/paloformation/service/ToDoServiceTest.java
@@ -1,35 +1,31 @@
 package fr.paloit.paloformation.service;
 
+
 import fr.paloit.paloformation.model.ToDo;
+import fr.paloit.paloformation.repository.SessionRepository;
+import fr.paloit.paloformation.repository.TacheRepository;
 import fr.paloit.paloformation.repository.ToDoRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.Optional;
 
+@SpringBootTest(classes = ToDoService.class)
 public class ToDoServiceTest {
 
-    @Test
-    public void todo(){
-        System.out.println("ToDoServiceTest.todo a");
-        Assertions.assertEquals(2, 3+1);
+    @Autowired
+    private ToDoService toDoService;
 
-    }
+    @MockBean
+    ToDoRepository toDoRepository;
 
-    @Test
-    public void todoa(){
-        System.out.println("ToDoServiceTest.todo b");
-        Assertions.assertEquals(2, 1+1);
-        System.out.println("ToDoServiceTest.todo c");
-    }
+    @MockBean
+    TacheRepository tacheRepository;
 
-    @Test
-    public void todob(){
-        System.out.println("ToDoServiceTest.todo d");
-        int x = 1/0;
-        System.out.println("ToDoServiceTest.todo c");
-    }
+    @MockBean
+    SessionRepository sessionRepository;
 
     @Test
     public void todoserv(){
@@ -43,74 +39,7 @@ public class ToDoServiceTest {
         Assertions.assertEquals(ToDo.Etat.FAIT,todo.getEtatb());
 
     }
-    ToDoService toDoService;
-    @BeforeEach
-    public void initialisation() {
-         toDoService= new ToDoService();
-        ToDoRepository toDoRepository=  new ToDoRepository(){
-          @Override
-          public <S extends ToDo> S save(S entity) {
-              return null;
-          }
 
-          @Override
-          public <S extends ToDo> Iterable<S> saveAll(Iterable<S> entities) {
-              return null;
-          }
-
-          @Override
-          public Optional<ToDo> findById(Long aLong) {
-              return Optional.empty();
-          }
-
-          @Override
-          public boolean existsById(Long aLong) {
-              return false;
-          }
-
-          @Override
-          public Iterable<ToDo> findAll() {
-              return null;
-          }
-
-          @Override
-          public Iterable<ToDo> findAllById(Iterable<Long> longs) {
-              return null;
-          }
-
-          @Override
-          public long count() {
-              return 0;
-          }
-
-          @Override
-          public void deleteById(Long aLong) {
-
-          }
-
-          @Override
-          public void delete(ToDo entity) {
-
-          }
-
-          @Override
-          public void deleteAllById(Iterable<? extends Long> longs) {
-
-          }
-
-          @Override
-          public void deleteAll(Iterable<? extends ToDo> entities) {
-
-          }
-
-          @Override
-          public void deleteAll() {
-
-          }
-      };
-        toDoService.toDoRepository = toDoRepository;
-
-    }
 
 
 }
