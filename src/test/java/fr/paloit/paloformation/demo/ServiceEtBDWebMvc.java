10fa29e4205743931f2eb9638098685b968c3cb6
Your Name <sfauvel@gmail.com>
2023-10-09 11:07:18 +0200

    Ajout d'un test avec le service et la base de données

---
+++ src/test/java/fr/paloit/paloformation/demo/ServiceEtBDWebMvc.java
@@ -0,0 +1,39 @@
+package fr.paloit.paloformation.demo;
+
+import fr.paloit.paloformation.model.Formation;
+import fr.paloit.paloformation.repository.FormationRepository;
+import fr.paloit.paloformation.service.FormationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test du service avec une vraie base de données
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ServiceEtBDWebMvc {
+
+    @Autowired
+    private FormationService formationService;
+
+    // Utilisation d'une vraie base de données.
+    @Autowired
+    FormationRepository formationRepository;
+
+    @Test
+    public void testAfficherFormations() throws Exception {
+        formationService.creerFormation(new Formation(null, "Test", Arrays.asList()));
+        final Iterable<Formation> formations = formationService.listeFormations();
+        final long count = StreamSupport.stream(formations.spliterator(), false).count();
+        assertEquals(1, count);
+
+    }
+
+}
