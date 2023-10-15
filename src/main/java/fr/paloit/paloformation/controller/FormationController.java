8ee3d5e37fb4b01fa32d54544ee208f47ec7173f
Pandora Pierrot <pandora.pierrot@gmail.com>
2023-10-15 22:05:52 +0200

    Ajout fragments + pure-min css

--- src/main/java/fr/paloit/paloformation/controller/FormationController.java
+++ src/main/java/fr/paloit/paloformation/controller/FormationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -13,6 +14,7 @@ public class FormationController {
 
     @Autowired
     FormationService formationService;
+
 
     @GetMapping({"/formations"})
     public String afficherFormations(Model model) {
@@ -27,12 +29,12 @@ public class FormationController {
     }
 
 
-    @PostMapping({"/formation-cree"})
-    public String formationCreee(@ModelAttribute Formation formation) throws Exception {
+
+@PostMapping({"/formation-cree"})
+    public String formationCreee(@ModelAttribute Formation formation){
         formationService.creerFormation(formation);
         return "redirect:/formations";
     }
-
 
     @GetMapping({"/formation/{id}"})
     public String detailFormation(Model model, @PathVariable Long id) {
