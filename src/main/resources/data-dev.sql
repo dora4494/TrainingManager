1d5460cb0a69a6256cee335ad2e57ce06e07e4ef
Your Name <sfauvel@gmail.com>
2023-10-23 11:50:33 +0200

    Ajout d'un script d'initialisation des données au démarrage

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,5 @@
+-- Insitialisation de données pour les environnements de développement
+
+insert into utilisateur (id,mail,nom,prenom) values (default,'sfauvel@palo-it.com','Fauvel','Sébastien')
+
+insert into formation (id, intitule) values (default, 'TDD')
