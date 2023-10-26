ca8bb7eb8028eff5db951eb9ab4f99c2e019d874
Pandora Pierrot <pandora.pierrot@gmail.com>
2023-10-26 10:40:36 +0200

    Correction erreur test SessionTest

--- src/test/java/fr/paloit/paloformation/ihm/SessionTest.java
+++ src/test/java/fr/paloit/paloformation/ihm/SessionTest.java
@@ -6,6 +6,7 @@ import fr.paloit.paloformation.model.Session;
 import fr.paloit.paloformation.model.ToDo;
 import fr.paloit.paloformation.model.Utilisateur;
 import fr.paloit.paloformation.service.SessionService;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
